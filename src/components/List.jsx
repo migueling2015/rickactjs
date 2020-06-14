2ce5719da44177b8ad26bfbc99ee281c36f4823d
migueling2015 <mprada.ing@gmail.com>
2020-06-14 17:42:10 -0500

    display: flex

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,26 +5,29 @@ import ListItem from './ListItem.jsx';
 import DoughnutPie from './DoughnutPieChart.jsx';
 
 import '../assets/styles/ProjectStyles.css'
+import '../assets/styles/ListContainerStyles.css'
 
 var dataToTransfer=[];
 
 const List=(props)=>{
     
     return(
-        <div className="list-container">
+        <div >
             <div>
                 <h3>{props.listTitle }</h3>
             </div>
-            <div>
-                <ul>
-                    {props.objectToList.map(character_json=>(
-                        character_json.map(character=>(
-                            <li key={character.id}>
-                                <ListItem   character_item={character}/>
-                            </li>
-                        ))
-                    ))}
-                </ul>
+            <div className="list-container">
+                <div>
+                    <ul>
+                        {props.objectToList.map(character_json=>(
+                            character_json.map(character=>(
+                                <li key={character.id}>
+                                    <ListItem   character_item={character}/>
+                                </li>
+                            ))
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     );
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -10,27 +10,29 @@ const ListItem=(props)=>{
     const IMG_URI='https://rickandmortyapi.com/api/character/avatar/'+ character_item.id +'.jpeg'
 
     return(
-        <div className="char-listItem-container">
-            <div className="char-data-box" width="200px" heigth="200px">
-                <div>
-                    <h2>{character_item.name}</h2>
-                </div>
-                <div>
-                    <h3>Spice: {character_item.species}</h3>
-                    {
-                        !character_item.type==""
-                            ?<h3>Type: {character_item.type}</h3>
-                            :""
-                    }
-                    <h3>Gender: {character_item.gender}</h3>
-                    <h3>Status: {character_item.status}</h3>
-                </div>
+        <div className="char-card">
+            <div className="char-name">
+                <h2>{character_item.name}</h2>
             </div>
-            <div>
-                {character_item.status==="Dead"
-                    ?<img className="char-img char-img-grayscale" src={IMG_URI} alt=""/>
-                    :<img className="char-img" src={IMG_URI} alt=""/>
-                }
+            <div className="char-listItem-container">
+                <div className="char-data-box">
+                    <div>
+                        <h3>Spice: {character_item.species}</h3>
+                        {
+                            !character_item.type==""
+                                ?<h3>Type: {character_item.type}</h3>
+                                :""
+                        }
+                        <h3>Gender: {character_item.gender}</h3>
+                        <h3>Status: {character_item.status}</h3>
+                    </div>
+                </div>
+                <div>
+                    {character_item.status==="Dead"
+                        ?<img className="char-img char-img-grayscale" src={IMG_URI} alt=""/>
+                        :<img className="char-img" src={IMG_URI} alt=""/>
+                    }
+                </div>
             </div>
         </div>
     );
